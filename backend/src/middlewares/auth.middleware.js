d38a74828b9bd297c5aaf4181f80149521e06d0b
Leonardo Guerini <leonardogguerrini@gmail.com>
2026-03-06 14:42:39 -0300

    atualização no auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,22 +1,20 @@
 import jwt from "jsonwebtoken"
-import { Usuario } from "../models/usuario.model"
 
 export async function authMiddleware(req, res, next) {
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    
     const secret = process.env.JWT_SECRET
 
-    if(!token) {
-        return res.status(401).json({ message: "Token ausente" })
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: "Bearer token ausente ou inválido" })
     }
 
-    jwt.verify(token, secret, (err, decoded) => {
-        if(err){
-            return res.status(403).json({ message: "Token inválido ou expirado" })
-        }
-    })
+    const token = authHeader && authHeader.split(' ')[1]
 
-    req.user = decoded
-    next()
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Token inválido ou expirado" })
+        }
+        req.user = decoded
+        next()
+    })
 }
