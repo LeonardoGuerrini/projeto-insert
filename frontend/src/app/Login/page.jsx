3142331c30eec9f6cc248e9d72b559254a1a853d
Leonardo Guerini <leonardogguerrini@gmail.com>
2026-03-27 14:09:28 -0300

    adiciona pagina de login

--- frontend/src/app/Login/page.jsx
+++ frontend/src/app/Login/page.jsx
@@ -12,10 +12,15 @@ export default function Login(){
         setUsuario("")
         setSenha("")
 
-        axios({
-            method: 'post',
-            url: 'http://localhost:8080/api/usuario/v1/'
-        })
+        try{
+            const response = await axios.post('http://localhost:8080/api/usuario/v1/login', {
+                usuario: `${usuario}`,
+                senha: `${senha}`
+            })
+            console.log(response)
+        } catch(err){
+            console.error(err)
+        }
     }
 
 
@@ -23,7 +28,7 @@ export default function Login(){
         <div>
             <form>
                 <label htmlFor="usuario">Usuário</label>
-                <input type="text" name="usuario" id="usuario" onChange={}/>
+                <input type="text" name="usuario" id="usuario" onChange={(e) => e.target.value(setUsuario)}/>
                 <label htmlFor="senha">Senha</label>
                 <input type="password" name="senha" id="senha" />
                 <button>Entrar</button>
